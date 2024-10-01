3d61b8b2de725fd6819b21b934ad803c4c4b25cd
Debabrat Swain <swaindebabratt@gmail.com>
2024-10-01 22:12:06 +0530

    initial commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
           setLoading(false);
           setSuccessMessage('Thank you for your message 😃');
           setErrorMessage('');
-          setForm({ name: '', email: '', message: '' }); // Reset form
+          setForm({ name: '', email: '', message: '' });
 
           setTimeout(() => {
             setSuccessMessage('');
@@ -58,19 +58,19 @@ const Contact = () => {
       <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50" />
 
       {/* Left Side Image */}
-      <div className="flex-shrink-0 w-full md:w-1/2 relative z-10 flex flex-col items-center justify-center p-8">
+      <div className="flex-shrink-0 w-full md:w-1/2 relative z-10 flex flex-col items-center justify-center p-4 md:p-8">
         <img 
           src="cheers.png" // Replace with your image URL
           alt="Chat"
-          className="w-48 h-48 rounded-lg shadow-lg"
+          className="w-32 h-32 md:w-48 md:h-48 rounded-lg shadow-lg"
         />
-        <h2 className="text-3xl md:text-4xl font-bold text-gold mb-4 text-center">Let's raise a glass together!</h2>
+        <h2 className="text-2xl md:text-3xl font-bold text-gold mb-4 text-center">Let's raise a glass together!</h2>
       </div>
 
       {/* Contact Form Section */}
-      <div className="relative z-10 flex items-center justify-center h-full p-8 w-full md:w-1/2">
-        <div className="p-8 rounded-lg shadow-lg w-full max-w-md">
-          <h1 className="mb-8 text-4xl md:text-5xl font-bold relative text-transparent bg-clip-text" style={{
+      <div className="relative z-10 flex items-center justify-center h-full p-4 md:p-8 w-full md:w-1/2">
+        <div className="p-4 md:p-8 rounded-lg shadow-lg w-full max-w-md">
+          <h1 className="mb-4 text-3xl md:text-4xl font-bold relative text-transparent bg-clip-text" style={{
             backgroundImage: "url('gold.avif')", // Replace with your texture image URL
             backgroundSize: 'cover',
           }}>
